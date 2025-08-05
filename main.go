71c7e9b622dbac68fe5b8374ac8e3d2d6e514a47
sebastian guayacan <guayacansebastian@gmail.com>
2025-08-05 12:46:17 -0500

    funciones

--- main.go
+++ main.go
@@ -5,31 +5,37 @@ import (
 	"strings"
 )
 
+const tickets = 50          //const, int
+var conference = "MY EVENT" //var, string
+var remaining int = 50.0    //var, int
+var userName string
+var lastName string
+var email string
+var userTickets int
+
 func main() {
 	// Declare variables and constants
-	var conference = "MY EVENT" //var, string
-	const tickets = 50          //const, int
-	var remaining int = 50.0    //var, int
-	var userName string         // var, string
+
+	// var userName string         // var, string
 	// var userTickets int
-	userTickets := 0 // shorthand (var), float64
+	// userTickets := 0 // shorthand (var), float64
 
 	// Prints
 	// fmt.Println("Welcome to", conference, "booking application")
 	// fmt.Printf("Get your tickets for the  %v event. \nWe have %v tickets  and %v are available.\n", conference, tickets, remaining)
 
-	greetings(conference, tickets)
-	fmt.Printf("UserName is %T, usertickts is %T\n", userName, userTickets) // %T prints the type of variable
-	fmt.Printf("Pointer example: %v\n", &userTickets)                       // & gives the memory address of variable
+	greetings()
+	// fmt.Printf("UserName is %T, usertickts is %T\n", userName, userTickets) // %T prints the type of variable
+	// fmt.Printf("Pointer example: %v\n", &userTickets)                       // & gives the memory address of variable
 	// user input
 	// fmt.Println("Enter your name:")
 	// fmt.Scan(&userName) // & gives the memory address of variable
 
-	var lastName string
+	// var lastName string
 	// fmt.Println("Enter your last name:")
 	// fmt.Scan(&lastName)
 
-	var email string
+	// var email string
 	// fmt.Println("Enter your email:")
 	// fmt.Scan(&email)
 
@@ -51,35 +57,12 @@ func main() {
 
 	//loops
 	for {
-		fmt.Println("Enter your name:")
-		fmt.Scan(&userName)
-		fmt.Println("Enter your last name:")
-		fmt.Scan(&lastName)
-		fmt.Println("Enter your email:")
-		fmt.Scan(&email)
-		fmt.Println("Enter number of tickets:")
-		fmt.Scan(&userTickets)
-		validname := len(userName) >= 2 && len(lastName) >= 2
-		validmail := strings.Contains(email, "@") && strings.Contains(email, ".com")
-		validnumber := userTickets <= remaining && userTickets > 0
+		userName, lastName, email, userTickets := get_user_input()
+
+		validname, validmail, validnumber := validadateuserinput(userName, lastName, email, userTickets, remaining)
 		//if condition
 		if validmail && validname && validnumber {
-
-			remaining = remaining - userTickets
-			fmt.Printf("Thank you %v for booking %v tickets.\n", userName, userTickets)
-			fmt.Printf("Slice of bookings: %v\n", bookingsSlice)
-			bookingsSlice = append(bookingsSlice, userName+" "+lastName)
-			firstnames := []string{}
-			for _, booking := range bookingsSlice { // range iterates over the array, range returns 2 values: index and value, if we don't need the index, we can use the underscore _
-				names := strings.Fields(booking)
-				firstname := names[0]
-				firstnames = append(firstnames, firstname)
-			}
-			fmt.Printf("First names of all bookings: %v\n", firstnames)
-
-			fmt.Printf("Thank you %v for booking %v tickets.\n", userName, userTickets)
-			fmt.Printf("%v tickets remaining for %v\n", remaining, conference)
-			fmt.Printf("You will receive a confirmation email at %v\n", email)
+			remaining, bookingsSlice = book_ticket(remaining, userTickets, userName, lastName, email, conference, bookingsSlice)
 
 		} else {
 			fmt.Println("Invalid input. Please try again.")
@@ -99,6 +82,7 @@ func main() {
 		}
 
 		// if condition
+		fmt.Printf("Remaining tickets: %v\n", remaining)
 		if remaining <= 0 {
 			fmt.Println("All tickets are sold out!")
 			break // exit the loop
@@ -117,8 +101,53 @@ func main() {
 	// 	fmt.Println("City not recognized.")
 	// }
 }
-func greetings(confname string, remainingtickets int) {
+func greetings() {
 
 	fmt.Println("Hello, wellcome to the application")
-	fmt.Printf("You can buy your tickets for %v event. There are %v tickets available.\n", confname, remainingtickets)
+	fmt.Printf("You can buy your tickets for %v event. There are %v tickets available.\n", conference, tickets) //We are using global variable here
+}
+func firstnames(bookings []string) []string {
+	firstnames := []string{}
+	for _, booking := range bookings { // range iterates over the array, range returns 2 values: index and value, if we don't need the index, we can use the underscore _
+		names := strings.Fields(booking)
+		firstname := names[0]
+		firstnames = append(firstnames, firstname)
+	}
+	return firstnames
+}
+
+func validadateuserinput(username string, lastname string, email string, usertickets int, remainingtickets int) (bool, bool, bool) {
+	validname := len(username) >= 2 && len(lastname) >= 2
+	validmail := strings.Contains(email, "@") && strings.Contains(email, ".com")
+	validnumber := usertickets <= remainingtickets && usertickets > 0
+	return validname, validmail, validnumber
+
+}
+
+func get_user_input() (string, string, string, int) {
+
+	fmt.Println("Enter your name:")
+	fmt.Scan(&userName)
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+	return userName, lastName, email, userTickets
+
+}
+func book_ticket(remaining int, userTickets int, userName string, lastName string, email string, conference string, bookingsSlice []string) (int, []string) {
+	remaining = remaining - userTickets
+	fmt.Printf("Thank you %v for booking %v tickets.\n", userName, userTickets)
+	fmt.Printf("Slice of bookings: %v\n", bookingsSlice)
+	bookingsSlice = append(bookingsSlice, userName+" "+lastName)
+	firstnames := firstnames(bookingsSlice)
+	fmt.Printf("First names of all bookings: %v\n", firstnames)
+
+	fmt.Printf("Thank you %v for booking %v tickets.\n", userName, userTickets)
+	fmt.Printf("%v tickets remaining for %v\n", remaining, conference)
+	fmt.Printf("You will receive a confirmation email at %v\n", email)
+	return remaining, bookingsSlice
+
 }
